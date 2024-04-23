1b7b144ff96ce121da861aa261e83d7d86c53568
KhuramC <khuramchoudhry123@gmail.com>
2024-04-22 21:47:27 -0500

    Added listener to gameChoicesBox and documentation for MVC architecture.

--- Final_Project/src/main/java/mainMenu/view/MainMenuStartView.java
+++ Final_Project/src/main/java/mainMenu/view/MainMenuStartView.java
@@ -5,7 +5,6 @@ import javax.swing.JPanel;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import mainMenu.model.GameType;
@@ -14,24 +13,33 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
 
+/**
+ * A View from the MVC architecture for the main menu to select a game to play.
+ * @author Khuram C.
+ */
 public class MainMenuStartView extends JFrame {
 	
 	private JPanel contentPane = new JPanel();
-	private JButton chooseGameButton;
+	private JButton startGameButton;
 	private JComboBox gameChoicesBox;
 
+	/**
+	 * Default constructor for the MainMenuStartView. Constructs the View and the associated objects necessary for the View.
+	 * Because it should look the same every time, there is only the default constructor.
+	 * @author Khuram C.
+	 */
 	public MainMenuStartView() {
-		setTitle("Game Pack(NAME TBD)"); // pick a better name
+		setTitle("Game Pack"); // pick a better name
 		setBounds(100,100,700,600);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.contentPane = new JPanel();
 		setContentPane(contentPane);
 		getContentPane().setLayout(null);
 		
-		this.chooseGameButton = new JButton("Choose Game");
-		chooseGameButton.setSize(188, 50);
-		chooseGameButton.setLocation(232, 301);
-		contentPane.add(chooseGameButton);
+		this.startGameButton = new JButton("Start Game");
+		startGameButton.setSize(188, 50);
+		startGameButton.setLocation(232, 301);
+		contentPane.add(startGameButton);
 		
 		this.gameChoicesBox = new JComboBox();
 		gameChoicesBox.setModel(new DefaultComboBoxModel(GameType.values()));
@@ -53,15 +61,30 @@ public class MainMenuStartView extends JFrame {
 		
 	}
 	
+	/**
+	 * Adds a listener to the gameChoicesBox for whenever a new game has been chosen.
+	 * @param listener to listen to gameChoicesBox.
+	 * @author Khuram C.
+	 */
+	public void addListenertoGameChoicesBox(ActionListener listener) {
+		gameChoicesBox.addActionListener(listener);
+	}
 	
-	public void addListenertoChooseGameButton(ActionListener listener) {
-		chooseGameButton.addActionListener(listener);
+	/**
+	 * Adds a listener to the startGameButton for whenever it gets pressed.
+	 * @param listener to listen to startGameButton.
+	 * @author Khuram C.
+	 */
+	public void addListenertoStartGameButton(ActionListener listener) {
+		startGameButton.addActionListener(listener);
 	}
 
-	public JButton getChooseGameButton() {
-		return chooseGameButton;
-	}
-	
+
+	/**
+	 * Returns the chosen GameType enum from the gameChoicesBox.
+	 * @return the GameType selected.
+	 * @author Khuram C.
+	 */
 	public GameType getGameChoicesBoxChoice() {
 		return (GameType) gameChoicesBox.getSelectedItem();
 	}
