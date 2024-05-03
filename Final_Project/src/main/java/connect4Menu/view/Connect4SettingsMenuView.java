df82ef0b3c2cdc615f3516a919cb1b5639dcf872
KhuramC <khuramchoudhry123@gmail.com>
2024-05-02 23:46:50 -0500

    Adjusted look of SettingsMenuView.
    Things that are supposed to be centered are now centered with each
    other. Supposed to fit same theme as mainMenu.

--- Final_Project/src/main/java/connect4Menu/view/Connect4SettingsMenuView.java
+++ Final_Project/src/main/java/connect4Menu/view/Connect4SettingsMenuView.java
@@ -10,14 +10,21 @@ import javax.swing.JRadioButton;
 import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 import javax.swing.JToggleButton;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeListener;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 
 import connect4Menu.model.Connect4MenuModel;
 import connect4Menu.model.Player1Colors;
@@ -27,6 +34,7 @@ import javax.swing.JTextArea;
 import java.awt.Font;
 import javax.swing.JInternalFrame;
 import java.awt.Color;
+import java.awt.Component;
 
 /**
  * A View from the MVC architecture used for choosing the settings of the
@@ -53,101 +61,220 @@ public class Connect4SettingsMenuView extends JFrame {
 	 * @author Khuram C.
 	 */
 	public Connect4SettingsMenuView() {
-		setTitle("Connect 4 Settings"); // general settings//
-		setBounds(100, 100, 900, 400);
+		// general settings//
+		setTitle("Connect 4 Settings"); 
+		int windowWidth = 900;
+		int windowHeight = 400;
+		setBounds(400, 100, windowWidth, windowHeight);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.contentPane = new JPanel();
-		//contentPane.setBackground(Color.BLACK);
+		contentPane.setBackground(Color.BLACK);
+		contentPane.setForeground(new Color(255, 215, 0));
 		setContentPane(contentPane);
 		getContentPane().setLayout(null);
 
 		
+		
+		int middleX = windowWidth/2;
+		//Title
+		int titleLabelWidth = 500;
+		int titleLabelHeight = 40;
+		int titleLabelY = 10;
+		JLabel titleLabel = new JLabel();
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		titleLabel.setOpaque(false);
+		titleLabel.setForeground(new Color(255, 215, 0));
+		titleLabel.setFont(new Font("Snap ITC", Font.ITALIC, 29));
+		titleLabel.setText("CONNECT4 SETTINGS");
+		titleLabel.setSize(titleLabelWidth, titleLabelHeight);
+		titleLabel.setLocation(middleX- titleLabelWidth/2, titleLabelY);
+		contentPane.add(titleLabel);
+		
+
+		int rulesTextPaneWidth = 500;
+		int rulesTextPaneHeight = 200;
+		int rulesTextPaneY = titleLabelHeight+titleLabelY+10;
+		JTextPane rulesTextPane = new JTextPane();
+		rulesTextPane.setSize(rulesTextPaneWidth,rulesTextPaneHeight);
+		rulesTextPane.setLocation(middleX-rulesTextPaneWidth/2, rulesTextPaneY);
+		rulesTextPane.setEditable(false);
+		rulesTextPane.setOpaque(false);
+		rulesTextPane.setForeground(new Color(255, 215, 0));
+		rulesTextPane.setFont(new Font("Britannic Bold", Font.PLAIN, 14));
+		StyledDocument documentStyle = rulesTextPane.getStyledDocument();
+		SimpleAttributeSet centerAttribute = new SimpleAttributeSet();
+		StyleConstants.setAlignment(centerAttribute, StyleConstants.ALIGN_CENTER);
+		documentStyle.setParagraphAttributes(0, documentStyle.getLength(), centerAttribute, false);
+		rulesTextPane.setText("Welcome to Connect4! The rules are simple:\n"
+				+ "Two players play turns in succession to try to get\n"
+				+ "four circles in a row\n"
+				+ "horizontally, vertically, or diagonally.\n"
+				+ "One's circle has to be played on top of another circle\n"
+				+ "if it is not going to the bottom row.\n"
+				+ "After a full column has been played on,\n"
+				+ "no more circles can be played on it.\n"
+				+ "Many different board sizes are available to choose from.\n"
+				+ "A timer can also be selected to force a turn\n"
+				+ " to be taken within 15 to 60 seconds.\n "
+				+ "Lastly, each player can configure what color they want.\n"
+				+ "Press 'Play Connect4' to start!");
+		
+		contentPane.add(rulesTextPane);
+		
+		int startGameButtonWidth = 200;
+		int startGameButtonHeight = 50;
+		startGameButton = new JButton("Play Connect4");
+		startGameButton.setSize(startGameButtonWidth, startGameButtonHeight);
+		startGameButton.setLocation(middleX-startGameButtonWidth/2, rulesTextPaneY+rulesTextPaneHeight + 20);
+		startGameButton.setForeground(new Color(255, 215, 0));
+		startGameButton.setBackground(Color.BLACK);
+		startGameButton.setFont(new Font("Britannic Bold", Font.PLAIN, 23));
+		startGameButton.setToolTipText("Click this to start a round of Connect4!");
+		contentPane.add(startGameButton);
+		
+		
+		int leftMiddleX = 115;
+		//sizeLabel
+		int sizeSettingsLabelWidth = 175;
+		int sizeSettingsLabelHeight = 20;
+		int sizeSettingsLabelY = 38;
+		JLabel sizeSettingsLabel = new JLabel("Size Settings (RowxCol)");
+		sizeSettingsLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		sizeSettingsLabel.setForeground(new Color(255, 215, 0));
+		sizeSettingsLabel.setFont(new Font("Britannic Bold", Font.ITALIC, 14));
+		sizeSettingsLabel.setSize(sizeSettingsLabelWidth,sizeSettingsLabelHeight);
+		sizeSettingsLabel.setLocation(leftMiddleX-sizeSettingsLabelWidth/2, sizeSettingsLabelY);
+		contentPane.add(sizeSettingsLabel);
+		
+		//RadioButtons//
 		String[] sizes = {"4x5","5x6","6x7(Standard)","7x8","8x8","7x9"};
-		int radioButtonWidth = 110;
+		int startingRadioButtonY = sizeSettingsLabelHeight+sizeSettingsLabelY + 5;
+		int radioButtonWidth = 130;
 		int radioButtonHeight= 20;
+		int finalRadioButtonY = 0;
 		for(int i = 0; i < sizes.length; i++) {
+			int radioButtonY = startingRadioButtonY+(radioButtonHeight*i);
+			UIManager.put("RadioButton.focus",new Color(255, 215, 0));
 			JRadioButton button = new JRadioButton(sizes[i]);
+			button.setOpaque(false);
+			button.setForeground(new Color(255, 215, 0));
+			button.setFont(new Font("Britannic Bold", Font.PLAIN, 13));
 			sizeButtonGroup.add(button);
 			button.setSize(radioButtonWidth, radioButtonHeight);
-			button.setLocation(63, 65+(radioButtonHeight*i));
+			button.setLocation(leftMiddleX-radioButtonWidth/2, radioButtonY);
+			finalRadioButtonY = radioButtonY;
+			
 			contentPane.add(button);
 		}
-
-		JTextField titleTextField = new JTextField();
-		titleTextField.setHorizontalAlignment(SwingConstants.CENTER);
-		titleTextField.setEditable(false);
-		titleTextField.setText("CONNECT4 SETTINGS");
-		titleTextField.setBounds(230, 10, 502, 39);
-		contentPane.add(titleTextField);
-		titleTextField.setColumns(10);
-
+		
+		//Timer
+		int timerToggleButtonWidth = 150;
+		int timerToggleButtonHeight = 30;
+		int timerToggleButtonY = finalRadioButtonY + 25;
+		UIManager.put("ToggleButton.select", new Color(255, 215, 0));
 		timerToggleButton = new JToggleButton("Enable Timer");
-		timerToggleButton.setBounds(53, 251, 115, 21);
+		timerToggleButton.setForeground(new Color(255, 215, 0));
+		timerToggleButton.setBackground(Color.BLACK);
+		timerToggleButton.setFont(new Font("Britannic Bold", Font.PLAIN, 15));
+		timerToggleButton.setToolTipText("Click this to start a round of Connect4!");
+		timerToggleButton.setSize(timerToggleButtonWidth, timerToggleButtonHeight);
+		timerToggleButton.setLocation(leftMiddleX-timerToggleButtonWidth/2, timerToggleButtonY);
 		contentPane.add(timerToggleButton);
 
+		
+		
+		int timerSliderWidth = 175;
+		int timerSliderHeight = 25;
+		int timerSliderY = timerToggleButtonY+50;
 		timerSlider = new JSlider();
-		timerSlider.setEnabled(false);
-		timerSlider.setMaximum(Connect4MenuModel.maxTimerTime);
-		timerSlider.setMinimum(Connect4MenuModel.minTimerTime);
-		timerSlider.setValue(Connect4MenuModel.minTimerTime);
 		timerSlider.setEnabled(false);
 		timerSlider.setVisible(false);
 		timerSlider.setBounds(10, 282, 200, 26);
+		timerSlider.setEnabled(false);
+		timerSlider.setOpaque(false);
+		timerSlider.setForeground(new Color(255, 215, 0));
+		timerSlider.setMaximum(Connect4MenuModel.maxTimerTime);
+		timerSlider.setMinimum(Connect4MenuModel.minTimerTime);
+		timerSlider.setValue(Connect4MenuModel.minTimerTime);
+		timerSlider.setSize(timerSliderWidth,timerSliderHeight);
+		timerSlider.setLocation(leftMiddleX-timerSliderWidth/2, timerSliderY);
 		contentPane.add(timerSlider);
 
+		
+		int timerTextFieldWidth = 25;
+		int timerTextFieldHeight = 20;
+		int timerTextFieldY = timerSliderY + 25;
 		timerTextField = new JTextField();
 		timerTextField.setText(Integer.toString(Connect4MenuModel.minTimerTime));
 		timerTextField.setEnabled(false);
 		timerTextField.setVisible(false);
-		timerTextField.setBounds(60, 318, 96, 19);
+		timerTextField.setFont(new Font("Britannic Bold", Font.PLAIN, 13));
+		timerTextField.setSize(timerTextFieldWidth, timerTextFieldHeight);
+		timerTextField.setLocation(leftMiddleX-timerTextFieldWidth/2, timerTextFieldY);
 		contentPane.add(timerTextField);
 		timerTextField.setColumns(10);
 
+		
+		int errorLabelWidth = 200;
+		int errorLabelHeight = 15;
+		int errorLabelY = timerTextFieldY + 35;
+		errorLabel = new JLabel("");
+		errorLabel.setFont(new Font("Britannic Bold", Font.ITALIC, 13));
+		errorLabel.setForeground(getForeground());
+		errorLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		errorLabel.setSize(errorLabelWidth, errorLabelHeight);
+		errorLabel.setLocation(leftMiddleX-errorLabelWidth/2, errorLabelY);
+		contentPane.add(errorLabel);
+		
+		
+		
+		int rightMiddleX = windowWidth - 115;
+		//Colors
+		int playerLabelWidth = 100;
+		int playerLabelHeight = 15;
+		int player1LabelY = 100; 
 		JLabel player1Label = new JLabel("Player 1 Colors");
-		player1Label.setBounds(761, 106, 93, 13);
+		player1Label.setFont(new Font("Britannic Bold", Font.ITALIC, 13));
+		player1Label.setForeground(getForeground());
+		player1Label.setHorizontalAlignment(SwingConstants.CENTER);
+		player1Label.setSize(playerLabelWidth, playerLabelHeight);
+		player1Label.setLocation(rightMiddleX-playerLabelWidth/2, player1LabelY);
 		contentPane.add(player1Label);
 
-		JLabel sizeSettingsLabel = new JLabel("Size Settings (RowxCol)");
-		sizeSettingsLabel.setBounds(53, 38, 147, 21);
-		contentPane.add(sizeSettingsLabel);
-
+		int comboBoxWidth = 100;
+		int comboBoxHeight = 25;
+		int player1ComboBoxY = player1LabelY+25;
 		JComboBox<PlayerColors> player1ColorsComboBox = new JComboBox<>();
 		player1ColorsComboBox.setModel(new DefaultComboBoxModel<>(Player1Colors.values()));
-		player1ColorsComboBox.setBounds(761, 128, 93, 21);
+		player1ColorsComboBox.setBackground(new Color(0, 0, 0));
+		player1ColorsComboBox.setForeground(new Color(255, 215, 0));
+		player1ColorsComboBox.setFocusable(false);
+		player1ColorsComboBox.setFont(new Font("Britannic Bold", Font.PLAIN, 15));
+		player1ColorsComboBox.setSize(comboBoxWidth, comboBoxHeight);
+		player1ColorsComboBox.setLocation(rightMiddleX-comboBoxWidth/2, player1ComboBoxY);
 		playerColorsComboBoxes.add(player1ColorsComboBox);
 		contentPane.add(player1ColorsComboBox);
 
+		int player2LabelY = player1ComboBoxY+100; 
 		JLabel player2Label = new JLabel("Player 2 Colors");
-		player2Label.setBounds(761, 210, 93, 13);
+		player2Label.setFont(new Font("Britannic Bold", Font.ITALIC, 13));
+		player2Label.setForeground(getForeground());
+		player2Label.setHorizontalAlignment(SwingConstants.CENTER);
+		player2Label.setSize(playerLabelWidth, playerLabelHeight);
+		player2Label.setLocation(rightMiddleX-playerLabelWidth/2, player2LabelY);
 		contentPane.add(player2Label);
 
+		int player2ComboBoxY = player2LabelY+25;
 		JComboBox<PlayerColors> player2ColorsComboBox = new JComboBox<>();
 		player2ColorsComboBox.setModel(new DefaultComboBoxModel<>(Player2Colors.values()));
-		player2ColorsComboBox.setBounds(761, 232, 93, 21);
+		player2ColorsComboBox.setBackground(new Color(0, 0, 0));
+		player2ColorsComboBox.setForeground(new Color(255, 215, 0));
+		player2ColorsComboBox.setFocusable(false);
+		player2ColorsComboBox.setFont(new Font("Britannic Bold", Font.PLAIN, 15));
+		player2ColorsComboBox.setSize(comboBoxWidth, comboBoxHeight);
+		player2ColorsComboBox.setLocation(rightMiddleX-comboBoxWidth/2, player2ComboBoxY);
 		playerColorsComboBoxes.add(player2ColorsComboBox);
 		contentPane.add(player2ColorsComboBox);
-
-		startGameButton = new JButton("Start Game");
-		startGameButton.setBounds(398, 298, 115, 21);
-		contentPane.add(startGameButton);
-
-		this.errorLabel = new JLabel("");
-		errorLabel.setBounds(10, 347, 200, 16);
-		contentPane.add(errorLabel);
-
-		JTextArea rulesTextArea = new JTextArea();
-		rulesTextArea.setEditable(false);
-		rulesTextArea.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
-		rulesTextArea.setLineWrap(true);
-		rulesTextArea.setText("Welcome to Connect4! The rules are simple. Two players play turns in succession to try"
-				+ " and get four in a row. One can get four in a row horizontally, vertically, and even diagonally."
-				+ " One's circle has to be played on top of another circle if it is not going to the bottom row."
-				+ " After a full column has been played on, no more circles can be played on it. Many different board"
-				+ " sizes are available to choose from. A timer can also be selected to force a turn to be taken within"
-				+ " 15 to 60 seconds of the other player's turn. Lastly, each player can configure what color they want."
-				+ " Once the settings have been decided, press 'Start Game' to start!");
-		rulesTextArea.setBounds(220, 64, 512, 203);
-		contentPane.add(rulesTextArea);
 	}
 
 	/**
@@ -289,8 +416,10 @@ public class Connect4SettingsMenuView extends JFrame {
 		errorLabel.setVisible(bool);
 		if (bool) {
 			timerToggleButton.setText("Disable Timer");
+			timerToggleButton.setForeground(Color.BLACK);
 		} else {
 			timerToggleButton.setText("Enable Timer");
+			timerToggleButton.setForeground(new Color(255, 215, 0));
 		}
 	}
 
