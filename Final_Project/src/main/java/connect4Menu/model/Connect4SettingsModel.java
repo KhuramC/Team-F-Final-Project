e34061431766309503bb35e47114a1f049b1ae97
KhuramC <khuramchoudhry123@gmail.com>
2024-05-03 00:21:10 -0500

    Implemented Observer Class in SettingsModel/more documentation.
    toggleTimer(used to be setTimer) and setTimerTime both notify observers
    of a change that occurs. Also added documentation on all methods in the
    model. ChangePlayerColor was changed to use getPlayer.

--- Final_Project/src/main/java/connect4Menu/model/Connect4SettingsModel.java
+++ Final_Project/src/main/java/connect4Menu/model/Connect4SettingsModel.java
@@ -1,12 +1,14 @@
 package connect4Menu.model;
 
+import java.util.Observable;
+
 /**
  * A model from the MVC architecture for the Connect4 menus to play a game of
  * Connect4.
  * 
  * @author Khuram C.
  */
-public class Connect4SettingsModel {
+public class Connect4SettingsModel extends Observable {
 
 	// default_values
 	private int colNum = 6; // x
@@ -21,19 +23,19 @@ public class Connect4SettingsModel {
 	public static final int player2Num = 2;
 
 	/**
-	 * A default constructor for the Connect4MenuModel. Because all the elements
-	 * already have a default value, no parameters are needed.
-	 * 
+	 * Returns the current amount of columns set for the game.
+	 * @return int number of columns.
 	 * @author Khuram C.
 	 */
-	public Connect4SettingsModel() {
-	}
-
-	// getters and setters
 	public int getColNum() {
 		return colNum;
 	}
 
+	/**
+	 * Returns the current amount of rows set for the game.
+	 * @return int number of rows.
+	 * @author Khuram C.
+	 */
 	public int getRowNum() {
 		return rowNum;
 	}
@@ -51,31 +53,77 @@ public class Connect4SettingsModel {
 		return true;
 	}
 
+	/**
+	 * Returns whether the timer is turned on or off before the game starts.
+	 * @return boolean for timer's status.
+	 * @author Khuram C.
+	 */
 	public boolean isTimer() {
 		return isTimer;
 	}
 
-	public void setTimer(boolean isTimer) {
-		this.isTimer = isTimer;
+	/**
+	 * Official feature method. Turns the timer into the other state, either turning it off or not.
+	 * Observers are notified of the change.
+	 * @return boolean detailing success.
+	 * @author Khuram C.
+	 */
+	public boolean toggleTimer() {
+		isTimer = !isTimer;
+		setChanged();
+		notifyObservers(isTimer);
+		clearChanged();
+		return true;
 	}
 
+	/**
+	 * Gives the current amount of time set for the timer.
+	 * @return int amount of timer set for the timer currently.
+	 * @author Khuram C.
+	 */
 	public int getTimerTime() {
 		return timerTime;
 	}
 
-	public void setTimerTime(int timerTime) {
+	/**
+	 * Sets the time of the timer and notifies Observers of the change.
+	 * @param timerTime time of timer.
+	 * @return boolean detailing success.
+	 * @author Khuram C.
+	 */
+	public boolean setTimerTime(int timerTime) {
 		this.timerTime = timerTime;
+		setChanged();
+		notifyObservers(timerTime);
+		clearChanged();
+		return true;
 	}
 
+	/**
+	 * Returns a player based on the number. Returns null if the input is invalid.
+	 * @param playerNum number of player.
+	 * @return player#playerNum or null.
+	 * @author Khuram C.
+	 */
 	public Player getPlayer(int playerNum) {
 		if(0<playerNum && playerNum<3)
 			return players[playerNum-1];
 		return null;
 	}
 	
+	/**
+	 * Changes the player Color based on the player's number. False is returned if unsuccessful due to an invalid input.
+	 * @param playerNum player#playerNum.
+	 * @param color to change to.
+	 * @return boolean detailing success or not.
+	 * @author Khuram C.
+	 */
 	public boolean changePlayerColor(int playerNum,PlayerColors color) {
-		players[playerNum-1].setColor(color);
-		return true;
+		Player player = getPlayer(playerNum);
+		if(player!=null) {
+			player.setColor(color);
+		}
+		return false;
 	}
 
 	
