043b4b96f2de494d3732d9f377f0e3436850ce4d
hipolitoIV <80554014+hipolitoIV@users.noreply.github.com>
2024-05-06 17:51:32 -0500

    Changed run() method in MancalaMain to correctly run

--- Final_Project/src/main/java/mancalaMenu/MancalaMain.java
+++ Final_Project/src/main/java/mancalaMenu/MancalaMain.java
@@ -8,13 +8,15 @@ public class MancalaMain {
 	private MancalaMain() {
 	}
 	
-    public static void startMancala() {
+	public static void startMancala() {
         SwingUtilities.invokeLater(new Runnable() {
         
         @Override
         public void run() { 
             MancalaModel model = new MancalaModel(); // add parameters later.
             MancalaController controller = new MancalaController(model);
+            MancalaView view = new MancalaView(controller);
+            controller.setView(view);
             controller.startGame();
         	}
         });
