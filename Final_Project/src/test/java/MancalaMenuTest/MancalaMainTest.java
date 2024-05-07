ac1923ce8cb7573fe416deda1545a6865ea4069b
hipolitoIV <80554014+hipolitoIV@users.noreply.github.com>
2024-05-06 23:46:41 -0500

    Manala test

--- Final_Project/src/test/java/MancalaMenuTest/MancalaMainTest.java
+++ Final_Project/src/test/java/MancalaMenuTest/MancalaMainTest.java
@@ -2,13 +2,16 @@ package MancalaMenuTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class MancalaMainTest {
+import mancalaMenu.*;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+public class MancalaMainTest {
+
 
 }
